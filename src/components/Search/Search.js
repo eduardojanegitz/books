9a8cd3d2a739da501ccb9ad574993eaa11782d75
Eduardo <edu0alves00@gmail.com>
2023-07-28 16:45:12 -0300

    fixing input style

---
+++ src/components/Search/Search.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  background-image: linear-gradient(90deg, #002f52 35%, #326589 160%);
+  color: #fff;
+  text-align: center;
+  padding: 85px 0;
+  height: 270px;
+  width: 100%;
+`;
+
+export const Title = styled.h2`
+  color: #fff;
+  font-size: 36px;
+  text-align: center;
+  width: 100%;
+`;
+export const Subtitle = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 40px;
+`;
